71dcab88b1bd3f9bdfe6a040bc7313ed88d80969
Meira-JH <jhalvesmeira@gmail.com>
2020-07-28 11:45:57 -0300

    subindo projeto

---
+++ futureEats/src/actions/page.test.js
@@ -0,0 +1,23 @@
+import { setCurrentPage, setSearch } from '../actions/page'
+
+
+describe ("Page Actions", () => {
+    test("Set Current Page", () => {
+        const mockSetCurrentPage = "Página selecionada"
+
+        const action = setCurrentPage(mockSetCurrentPage);
+
+        //verificação
+        expect(action.type).toBe("SET_CURRENT_PAGE");
+        expect(action.payload.currentPage).toBe(mockSetCurrentPage)
+
+    })
+    test("Set Search", () => {
+        const mockSearch = "Sou uma pesquisa"
+
+        const action = setSearch(mockSearch);
+
+        expect(action.type).toBe("SET_SEARCH");
+        expect(action.payload.currentSearch).toBe(mockSearch)
+    })
+})
